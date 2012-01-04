321d72fd41b7854c8a544def8f33b15a15e108bf
Matt Harrison <matt61408@gmail.com>
2012-01-04 14:27:19 -0500

    Add Sort Playlist options to sort by Artist, Album, or Track Name
    Functionized some of the actions to simplify the code somewhat

--- rdio-enhancer.js
+++ rdio-enhancer.js
@@ -23,40 +23,72 @@ function injectedJs() {
 				// if this is an Album
 				if(datakey === 'a') {
 					item.menu_items.splice(3, 0,
-								{
-									title: "Add Album to Playlist",
-									visible: function() {
-										return true;
-									},
-									action: function() {
-										var copy = jQuery.extend(true, {}, data);
-										copy.key = data.trackKeys;
-										R.Playlists.showAddToPlaylistDialog(copy);
-										return false;
-									}
-								});
+					{
+						title: "Add Album to Playlist",
+						visible: function() {
+							return true;
+						},
+						action: function() {
+							var copy = jQuery.extend(true, {}, data);
+							copy.key = data.trackKeys;
+							R.Playlists.showAddToPlaylistDialog(copy);
+							return false;
+						}
+					});
 				}
 				// if this is a Playlist
 				else if(datakey === 'p') {
-					item.menu_items.splice(0, 0,
-								{
-									title: "Fork Playlist",
-									visible: function() {
-										return true;
-									},
-									action: function() {
-										var copy = jQuery.extend(true, {}, data);
-										copy.key = [];
-										for(key in data.tracks) {
-											copy.key.push(data.tracks[key].key);
-										}
-										R.Playlists.showAddToPlaylistDialog(copy);
-										return false;
-									}
-								},
-								{
-									type: "separator"
-								});
+					if(data.tracks) {
+						item.menu_items.splice(0, 0,
+						{
+							title: "Fork Playlist",
+							visible: function() {
+								return true;
+							},
+							action: function() {
+								var copy = jQuery.extend(true, {}, data);
+								copy.key = getKeysFromTracks( data.tracks );
+								R.Playlists.showAddToPlaylistDialog(copy);
+								return false;
+							}
+						},
+						{
+							type: "separator"
+						},
+						{
+							title: "Sort Playlist by Artist",
+							visible: function() {
+								return true;
+							},
+							action: function() {
+								sortPlaylist(data.key, getKeysFromTracks( data.tracks.sort(sortByArtist) ));
+								return false;
+							}
+						},
+						{
+							title: "Sort Playlist by Album",
+							visible: function() {
+								return true;
+							},
+							action: function() {
+								sortPlaylist(data.key, getKeysFromTracks( data.tracks.sort(sortByAlbum) ));
+								return false;
+							}
+						},
+						{
+							title: "Sort Playlist by Track Name",
+							visible: function() {
+								return true;
+							},
+							action: function() {
+								sortPlaylist(data.key, getKeysFromTracks( data.tracks.sort(sortByTrackName) ));
+								return false;
+							}
+						},
+						{
+							type: "separator"
+						});
+					}
 				}
 				// This is something new
 				else {
@@ -73,15 +105,95 @@ function injectedJs() {
 	R.Api.origRequest = R.Api.request;
 	R.Api.request = function() {
 		var args = arguments[0];
+		//console.log("Request");
+		//console.log(arguments);
 		if (args.method == 'addToPlaylist' || args.method == 'createPlaylist') {
 			var tracks = args.content.tracks;
 			if (tracks.length == 1 && tracks[0] instanceof Array) {
 				args.content.tracks = args.content.tracks[0];
-				R.Api.request(args);
-				return;
+				return R.Api.request(args);
 			}
 		}
 		return R.Api.origRequest.apply(this, arguments);
+	};
+	
+	// Sort functions
+	var sortByArtist = function(a, b) {
+		var artist_a = a.albumArtist.toLowerCase(),
+		artist_b = b.albumArtist.toLowerCase();
+		if (artist_a < artist_b) {
+			return -1;
+		}
+		else if (artist_a > artist_b) {
+			return 1;
+		}
+		else {
+			return sortByAlbum(a, b);
+		}
+	},
+	sortByAlbum = function(a, b) {
+		var album_a = a.album.toLowerCase(),
+		album_b = b.album.toLowerCase();
+		if (album_a < album_b) {
+			return -1;
+		}
+		else if (album_a > album_b) {
+			return 1;
+		}
+		else {
+			return sortByTrackNum(a, b);
+		}
+	},
+	sortByTrackName = function(a, b) {
+		var trackname_a = a.name.toLowerCase(),
+		trackname_b = b.name.toLowerCase();
+		if (trackname_a < trackname_b) {
+			return -1;
+		}
+		else if (trackname_a > trackname_b) {
+			return 1;
+		}
+		else {
+			return sortByTrackNum(a, b);
+		}
+	},
+	sortByTrackNum = function(a, b) {
+		if (a.trackNum < b.trackNum) {
+			return -1;
+		}
+		else if (a.trackNum > b.trackNum) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	},
+	
+	//
+	sortPlaylist = function(key, tracks) {
+		R.Api.request({
+			method:"setPlaylistOrder",
+			content: {
+				playlist:key,
+				tracks:tracks
+			},
+			success: function(status) {
+				if (status.result) {
+					R.Notifications.show('The playlist has been sorted successfully. Please reload it to view the changes.');
+				}
+				else {
+					R.Notifications.show('You do not have permission to sort this playlist.');
+				}
+			}
+		});
+	},
+	
+	getKeysFromTracks = function(tracks) {
+		var keys = [];
+		for(key in tracks) {
+			keys.push(tracks[key].key);
+		}
+		return keys;
 	};
 }
 
